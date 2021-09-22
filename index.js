ddc79c2e9efa33e18c124a7edcd90785c45aa158
Adam <inmon.adam@yahoo.com>
2021-09-21 23:26:54 -0500

    commit tree 614e59166c6435582efe2622b89479cf159d6783

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution in this file!
+const hq = 42;
+const feet = 264;
+
+function distanceFromHqInBlocks(pickup){
+    if (pickup > hq) {
+        return pickup - hq
+    } else if (pickup < hq) {
+        return hq - pickup
+    }
+
+}
+
+function distanceFromHqInFeet(location){
+    return distanceFromHqInBlocks(location) * 264;
+}
+
+function distanceTravelledInFeet(a, b){
+    const block = 264;
+    if (b > a){
+        return (b * block) - (a * block);
+    }
+    else if (a > b){
+        return (a * block) - (b * block);
+    }
+}
+
+function calculatesFarePrice(a, b){
+   let value = distanceTravelledInFeet(a, b);
+    if (value <= 400){
+        return 0;
+    } else if (value <= 2000){
+        return ((value - 400) * 0.02);
+    } else if (value <= 2500){
+        return 25;
+    } else if (value > 2500){
+        return 'cannot travel that far';
+    }
+}
